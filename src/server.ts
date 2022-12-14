79dcb08bce00097c4ecca78ae7e420412dfbe939
Yongchang He <80454212+hyc0812@users.noreply.github.com>
2022-12-14 11:12:54 -0600

    Finished the external adapter (server)

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,6 @@ import bodyParser = require("body-parser");
 import axios, { AxiosResponse } from "axios";
 
 
-
 type EAInput = {
     id: number | string;
     data: {
@@ -13,7 +12,7 @@ type EAInput = {
     };
 };
 
-type EAoutput = {
+type EAOutput = {
     jobRunId: string | number;
     statusCode: number;
     data: {
@@ -31,9 +30,34 @@ app.get("/", (req: Request, res: Response) => {
     res.send("External Adapter says Hellooooo!");
 });
 
-app.post("/", (req: Request, res: Response) => {
+app.post("/", async (req: Request, res: Response) => {
     const eaInputData: EAInput = req.body;
     console.log("Request data received : ", eaInputData);
+
+    // build the API request to look something like http://numbersapi.com/random/trivia
+    const url = `http://numbersapi.com/${eaInputData.data.number}/${eaInputData.data.infoType}`;
+
+    // build EA' response
+    let eaResponse: EAOutput = {
+        data: {},
+        jobRunId: eaInputData.id,
+        statusCode: 0,
+    };
+
+    try {
+        const apiResponse: AxiosResponse = await axios.get(url);
+        eaResponse.data = {result: apiResponse.data};
+        eaResponse.statusCode = apiResponse.status;
+        console.log("RETURNED response: ", eaResponse);
+        res.json(eaResponse);
+    }catch(error: any) {
+        console.log("API Response error: ", error);
+        eaResponse.error = error.message;
+        eaResponse.statusCode = error.response.status;
+        res.json(eaResponse);
+    }
+
+
 });
 
 app.listen(PORT, () => {
